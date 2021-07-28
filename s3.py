078427e7e00be398fcb1fd59ad886ccacdfd5155
Gogs <gogs@fake.local>
2021-07-28 08:33:00 +0700

    Add download functionality, breakdown list into iter and list

--- s3.py
+++ s3.py
@@ -10,16 +10,19 @@ from botocore.exceptions import ClientError
 
 
 class S3Client:
+    """
+    Some terminologies:
+        fout: file name "out" there, i.e in the server (S3), or in S3 term, key.
+        fin: file name "in" here, i.e local machine.
+    """
 
     def __init__(self, config_path):
-
         self.config = self._load_config(config_path)
         self.client = boto3.client("s3", 
                     aws_access_key_id=self.config['credentials']['secret_key'],
                     aws_secret_access_key=self.config['credentials']['access_key'],
                     endpoint_url=self.config['credentials']['endpoint_url']
                 )
-        # self.bucket = self.client.Bucket(config['credentials']['bucket'])
 
     def _load_config(self, config_path):
         config = configparser.ConfigParser()
@@ -30,7 +33,25 @@ class S3Client:
         pass
 
     def download(self, fout, fin, w_encrypt=False):
-        pass
+        if w_encrypt:
+            encrypt_args = {
+                    "SSECustomerKey": self.config['encryption']['key'],
+                    "SSECustomerAlgorithm": self.config['encryption']['algo']
+                }
+        else:
+            encrypt_args = {}
+
+        try:
+            response = self.client.download_file(
+                Key=fout,
+                Bucket=self.config['credentials']['bucket'],
+                Filename=fin,
+                ExtraArgs = encrypt_args
+            )
+        except ClientError as e:
+            print(e)
+        else:
+            print("Download successfull")
 
     def delete(self, fout):
         pass
@@ -38,14 +59,22 @@ class S3Client:
     def move(self, fout_src, fout_tgt):
         pass
 
-    def list(self, pattern=None):
+    def iter(self, pattern=None):
         response = self.client.list_objects_v2(Bucket=self.config['credentials']['bucket'])
         assert response['ResponseMetadata']['HTTPStatusCode'] == 200,\
                 'List object failed, {}'.format(response)
 
         for obj in response['Contents']:
-            print (obj['Key']) 
+            yield obj
+
+    def list(self, pattern=None):
+        fouts = []
+        for obj in self.iter():
+            fouts.append(obj)
+        return fouts
 
 if __name__ == "__main__":
     client = S3Client('config.yml')
-    client.list()
+    fouts = client.list()
+    print("\n".join([o['Key'] for o in fouts]))
+    client.download(fouts[0]['Key'], fouts[1]['Key'], w_encrypt=True)
