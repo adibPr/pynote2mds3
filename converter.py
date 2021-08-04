7842c80056bb5bb7bfdbb1f11410ea2dc012320e
Gogs <gogs@fake.local>
2021-08-04 09:06:34 +0700

    Skeleton for converter

---
+++ converter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os
+import sys
+
+class Notebook(object):
+
+    def __init__(self):
+        pass
+
+    def _upload(self, obj):
+        pass
+
+    def _replace_url(self, cell, obj):
+        pass
+
+    def convert(self, notes):
+        pass
+
+    def test_validity(self, notes):
+        pass
+
